63f0d9d9c12db35b82c985adfea52d55239d3d55
Chintal Dholakiya <chintal3198@gmail.com>
2023-02-12 11:26:30 +0100

    changed hero section description

--- frontend/src/components/hero.js
+++ frontend/src/components/hero.js
@@ -28,9 +28,10 @@ export default function Hero() {
           color="text.secondary"
           paragraph
         >
-          Your link will be more remembered than ever before. A short URL is
-          beneficial to fit into your portfolio, whether you're communicating
-          with prospects offline or online.
+          Your affiliate link or your portfolio link is too long ? You want to
+          make it shorter? URL Shortener is at your service! Just enter your
+          long URL and get a short URL. When user clicks it, they will be
+          redirected to original link. Tada!!
         </Typography>
       </Container>
     </Box>
